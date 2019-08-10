068e0c4a6023600c393ef526f760c7976845afda
Qi Zhang <singularitti@outlook.com>
2019-08-10 07:46:58 -0400

    Add some code to PyShlex.jl

--- src/PyShlex.jl
+++ src/PyShlex.jl
@@ -1,5 +1,29 @@
 module PyShlex
 
-greet() = print("Hello World!")
+using Parameters
+
+export Lexer
+
+const WHITESPACE = raw" \t\r\n"
+const QUOTES = raw"'\""
+const ESCAPE = raw"\\"
+const ESCAPED_QUOTES = raw"\""
+
+@with_kw struct Lexer
+    instream::IO
+    state::AbstractChar = " "
+    lineno::Int = 1
+    token = ""
+    punctuation_chars = ""
+end
+
+function get_token(lexer::Lexer)
+
+end # function get_token
+
+function read_token(lexer::Lexer)
+    quoted = false
+    escaped_state = " "
+end # function read_token
 
 end # module
